aa033d8958feacac3ab27d360bc047a8f1fa7dc2
Marjorie Entoma <19104863@usc.edu.ph>
2021-05-07 10:39:55 +0800

    Landing page

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Tea'kTalker</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+
+  <link href="../assets/css/style.css" rel="stylesheet">
+
+ 
+</head>
+
+<body>
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+     
+          <div class="carousel-item active">
+            <div class="carousel-background" ><img src="../img/slide-2.jpg" style="width:100%;height:750px" alt="" ></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated animate__fadeInDown"></h2>
+                <h1 class="animate__animated animate__fadeInUp " >𝙍𝙚𝙛𝙧𝙚𝙨𝙝 𝙖𝙣𝙙 𝙧𝙚𝙛𝙧𝙚𝙨𝙝 𝙮𝙤𝙪𝙧𝙨𝙚𝙡𝙛 𝙬𝙞𝙩𝙝 𝙢𝙞𝙡𝙠𝙩𝙚𝙖 f𝐥𝐚𝐯𝐨𝐫𝐬 𝐭𝐡𝐚𝐭 𝐰𝐢𝐥𝐥 𝐥𝐢𝐟𝐭 𝐲𝐨𝐮 𝐨𝐟𝐟 𝐲𝐨𝐮𝐫 𝐟𝐞𝐞𝐭 </h1>
+                <a href="form.php" class="btn-get-started animate__animated animate__fadeInUp scrollto">Get Started</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section><!-- End Hero -->
+
+  
+  
+
+
+
+</body>
+
+</html>
